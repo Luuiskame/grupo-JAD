bf8f752ab1bca746dd8d07891e32ed63fc80c9e5
Luis Reyes <yuiftw05@gmail.com>
2025-04-08 09:47:45 -0600

    feat: template done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import astroIcon from 'astro-icon';
 
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(),
+    astroIcon()
+  ]
 });
